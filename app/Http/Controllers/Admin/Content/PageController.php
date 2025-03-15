79b465434ccd307dbaae4e52ae10467d9b4065e1
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-15 22:46:21 +0330

    complete PageController.php

--- app/Http/Controllers/Admin/Content/PageController.php
+++ app/Http/Controllers/Admin/Content/PageController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Admin\Content;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StorePageRequest;
+use App\Http\Requests\Admin\UpdatePageRequest;
 use App\Http\Resources\Admin\Content\PageCollection;
 use App\Http\Resources\Admin\Content\PageResource;
+use App\Http\Responses\ApiJsonResponse;
 use App\Models\Content\Page;
-use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
@@ -32,24 +33,28 @@ class PageController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Page $page)
     {
-        //
+        return new PageResource($page);
     }
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdatePageRequest $request, Page $page)
     {
-        //
+        $inputs = $request->all();
+        $page->update($inputs);
+        return new PageResource($page);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Page $page)
     {
-        //
+        $page->delete();
+        return ApiJsonResponse::success(null, 'منو حذف شد');
     }
 }
