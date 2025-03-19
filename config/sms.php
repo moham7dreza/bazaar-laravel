b786afd67fcc1a7880cdb512daedfc24b5a2ec19
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-19 22:29:14 +0330

    add sms service

---
+++ config/sms.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'username' => env('SMS_USERNAME ', '09932932392'),
+    'password' => env('SMS_PASSWORD ', 'swxwdxcedcxde'),
+    'from' => env('SMS_USERNAME ', '50000000321'),
+];
