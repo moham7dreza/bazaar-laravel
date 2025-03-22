0d12a565a76898fbf4dcf020e473759489e46d40
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-22 15:00:02 +0330

    add trans for response messages

---
+++ lang/en/response.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'image' => [
+        'upload failed' => 'image upload operation failed, try again later'
+    ],
+    'general' => [
+        'successful' => 'operation done',
+        'failed' => 'operation failed',
+    ]
+];
