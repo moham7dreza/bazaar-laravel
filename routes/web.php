f7a564a3a5175e18db4d8cde8c1be362c15445bc
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-23 16:28:58 +0330

    add log viewer package and inline blade for access important services

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,12 @@ use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return <<<'blade'
+        <div style="display: flex;flex-direction: column;gap: 1rem;font-size: 2rem;">
+            <a href="/dosc/api">api docs</a>
+            <a href="/log-viewer">log viewer</a>
+        </div>
+    blade;
 });
 
 // Route::get('image', [ImageController::class, 'index']);
