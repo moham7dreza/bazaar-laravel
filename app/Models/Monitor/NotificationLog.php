cf2009d1521bc635752840886cf4c89f62e0765b
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-31 23:10:23 +0330

    sms and notification mongo models

---
+++ app/Models/Monitor/NotificationLog.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Monitor;
+
+use MongoDB\Laravel\Eloquent\Model;
+
+class NotificationLog extends Model
+{
+    protected $collection = 'notification_logs';
+
+    protected $connection = 'mongodb';
+
+    protected $guarded = [];
+}
---
+++ app/Models/Monitor/SmsLog.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\Monitor;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use MongoDB\Laravel\Eloquent\Model;
+
+class SmsLog extends Model
+{
+    protected $collection = 'sms_logs';
+
+    protected $connection = 'mongodb';
+
+    protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+//            'connector' => SmsProviderEnum::class,
+//            'status' => SmsStatusEnum::class,
+//            'type' => SmsTypeEnum::class,
+//            'message_type' => SmsMessageTypeEnum::class,
+            'sent_at' => 'datetime',
+            'delivered_at' => 'datetime',
+        ];
+    }
+}
