6ed8fdcd5f945ea979b5fc0c4f08529927557ad8
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-03-31 14:52:17 +0330

    setup parallel testing

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HealthServiceProvider::class,
+    \Tests\TestsServiceProvider::class,
 ];
---
+++ tests/TestGroup.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tests;
+
+class TestGroup
+{
+    public const MAIN_FLOW = 'main-flow';
+}
