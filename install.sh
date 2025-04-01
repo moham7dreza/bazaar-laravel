46ed495f7a60bca5d6cc7b9ebee0093e02cccd27
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-01 11:21:46 +0330

    add scripts

---
+++ install.sh
@@ -0,0 +1,35 @@
+# Check for optional parameter
+skip_composer=false
+skip_horizon=true
+
+for arg in "$@"; do
+    case $arg in
+        "--c")
+            skip_composer=true
+            ;;
+        "--h")
+            skip_horizon=true
+            ;;
+        *)
+            # Unknown argument
+            ;;
+    esac
+done
+
+git pull
+if [ "$skip_composer" = false ]; then
+    ./composer.phar install --optimize-autoloader --no-dev
+    #this is moved from post-install-cmd to post-install inorder to not executed automatically
+    # and use docker cache to speed up build process
+    ./composer.phar run-script post-install
+    ./composer.phar dump-autoload --optimize
+fi
+php artisan clear-compiled
+php artisan optimize
+php artisan event:cache
+php artisan view:cache
+php artisan migrate --force
+
+if [ "$skip_horizon" = false ]; then
+    sudo supervisorctl restart horizon
+fi
