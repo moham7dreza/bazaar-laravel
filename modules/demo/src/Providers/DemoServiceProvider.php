cb5547fc4d42f0528a2794949bb39366f2dc00c1
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-01 11:45:37 +0330

    add demo module

---
+++ modules/demo/src/Providers/DemoServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Demo\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DemoServiceProvider extends ServiceProvider
+{
+	public function register(): void
+	{
+	}
+	
+	public function boot(): void
+	{
+	}
+}
---
+++ modules/demo/tests/Feature/Providers/DemoServiceProviderTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Demo\Tests;
+
+use Tests\TestCase;
+
+class DemoServiceProviderTest extends TestCase
+{
+	// TODO
+}
