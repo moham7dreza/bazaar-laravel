f2953b1179f7632409a24d51fd2a0094e43097a5
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-03 14:12:36 +0330

    fix

--- app/Providers/Filament/SuperAdminPanelProvider.php
+++ app/Providers/Filament/SuperAdminPanelProvider.php
@@ -90,7 +90,7 @@ class SuperAdminPanelProvider extends PanelProvider
 
     private function getNavItems(): array
     {
-        return collect(config('tools'))
+        return collect(config('tools'))->except('backend-admin')
             ->map(fn (array $tool) => NavigationItem::make()
                 ->label(fn (): string => trans($tool['title']))
                 ->url($tool['url'], shouldOpenInNewTab: true)
