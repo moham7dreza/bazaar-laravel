738581992423c6311b91fd0b41b3329bc6e4cee5
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-03 13:34:53 +0330

    add global LatestScope

--- app/Models/Monitor/CommandPerformanceLog.php
+++ app/Models/Monitor/CommandPerformanceLog.php
@@ -2,10 +2,14 @@
 
 namespace App\Models\Monitor;
 
+use App\Models\Scopes\LatestScope;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Prunable;
 
+#[ScopedBy([LatestScope::class])]
 class CommandPerformanceLog extends Model
 {
     use Prunable;
@@ -21,7 +25,8 @@ class CommandPerformanceLog extends Model
         return static::where('created_at', '<=', now()->subDays(14));
     }
 
-    public function scopeHighestQueryTime($query, int $count = 1000): Builder
+    #[Scope]
+    public function highestQueryTime($query, int $count = 1000): Builder
     {
         return $query->where('query_time', '>=', $count);
     }
