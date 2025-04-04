3cb7bbd38caeaa1d3fc6f1e30e7feb9f77ad4acb
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-04 12:17:35 +0330

    add factory for adv note and refactor model

--- app/Models/Advertise/AdvertisementNote.php
+++ app/Models/Advertise/AdvertisementNote.php
@@ -2,18 +2,32 @@
 
 namespace App\Models\Advertise;
 
+use App\Models\Scopes\LatestScope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[ScopedBy([LatestScope::class])]
 class AdvertisementNote extends Model
 {
-    use HasFactory, SoftDeletes;
+    /*** _____________________________________________ use SECTION ________________________________________________ ***/
+    use HasFactory;
+    use SoftDeletes;
 
+    /*** _____________________________________________ props SECTION ______________________________________________ ***/
     protected $guarded = ['id'];
 
-    public function advertisement()
+    /*** _____________________________________________ model related methods SECTION ______________________________ ***/
+
+    /*** _____________________________________________ relations SECTION __________________________________________ ***/
+
+    public function advertisement(): BelongsTo
     {
         return $this->belongsTo(Advertisement::class);
     }
+
+    /*** _____________________________________________ method SECTION __________________________________________ ***/
+
 }
--- database/factories/AdvertisementNoteFactory.php
+++ database/factories/AdvertisementNoteFactory.php
@@ -2,22 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\Advertise\Advertisement;
+use App\Models\Advertise\AdvertisementNote;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
- */
 class AdvertisementNoteFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = AdvertisementNote::class;
+
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'advertisement_id' => Advertisement::factory(),
+            'note' => $this->faker->text,
         ];
     }
 }
