a2f5260b2f65a550f4447de23035272e3917c1ac
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-04 12:22:58 +0330

    add factory for category and refactor model

--- app/Models/Advertise/Category.php
+++ app/Models/Advertise/Category.php
@@ -2,17 +2,31 @@
 
 namespace App\Models\Advertise;
 
+use App\Models\Scopes\LatestScope;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Dyrynda\Database\Support\CascadeSoftDeletes;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[ScopedBy([LatestScope::class])]
 class Category extends Model
 {
-    use CascadeSoftDeletes, HasFactory, Sluggable, SoftDeletes;
+    /*** _____________________________________________ use SECTION ________________________________________________ ***/
+    use CascadeSoftDeletes;
+    use HasFactory;
+    use Sluggable;
+    use SoftDeletes;
 
-    protected $cascadeDeletes = ['children'];
+    /*** _____________________________________________ props SECTION ______________________________________________ ***/
+    protected $guarded = ['id', 'slug'];
+
+    protected array $cascadeDeletes = ['children'];
+
+    /*** _____________________________________________ model related methods SECTION ______________________________ ***/
 
     public function sluggable(): array
     {
@@ -23,25 +37,27 @@ class Category extends Model
         ];
     }
 
-    protected $guarded = ['id', 'slug'];
-
-    public function children()
+    /*** _____________________________________________ relations SECTION __________________________________________ ***/
+    public function children(): HasMany
     {
         return $this->hasMany($this, 'parent_id');
     }
 
-    public function parent()
+    public function parent(): BelongsTo
     {
         return $this->belongsTo($this, 'parent_id');
     }
 
-    public function advertisements()
+    public function advertisements(): HasMany
     {
         return $this->hasMany(Advertisement::class);
     }
 
-    public function attributes()
+    public function attributes(): HasMany
     {
         return $this->hasMany(CategoryAttribute::class);
     }
+
+    /*** _____________________________________________ method SECTION __________________________________________ ***/
+
 }
--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -2,22 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Models\Advertise\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
- */
 class CategoryFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Category::class;
+
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
+            'icon' => 'fa fa-car',
+            'parent_id' => $this->faker->boolean ? Category::factory() : null,
+            'status' => $this->faker->boolean,
         ];
     }
 }
