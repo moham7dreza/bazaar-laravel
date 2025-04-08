ee27ef41a0726f506c5af0ca465809116a253766
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-09 00:08:25 +0330

    fix middleware

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum', 'mobileVerified'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
