369de1d5868d556e188af4f90ba7161db29eb983
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-09 23:55:14 +0330

    add user act methods to Pest

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Enums\UserRole;
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 pest()->extend(Tests\TestCase::class)
     ->use(DatabaseTransactions::class)
     ->in('Feature', 'EndToEnd', '../modules/*/tests');
+
+function asUser(User $user): \Tests\TestCase
+{
+    return test()->be($user);
+}
+
+function asAnAuthenticatedUser(): \Tests\TestCase
+{
+    $user = User::factory()->create();
+
+    return asUser($user);
+}
+
+function asAdminUser(User $user): \Tests\TestCase
+{
+    $user->assignRole(UserRole::ADMIN);
+
+    return test()->be($user);
+}
