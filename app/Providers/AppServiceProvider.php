6d086173a239467a6f28d5079ea6711348da85d3
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-10 13:00:29 +0330

    Model::automaticallyEagerLoadRelationships()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+
+        Model::automaticallyEagerLoadRelationships();
 
         $this->setupGates();
     }
