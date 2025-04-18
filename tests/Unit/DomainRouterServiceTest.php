8c29e9232018197b79fe7151502fcbabee9b832b
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-18 10:32:25 +0330

    :recycle: refactor DomainRouterServiceTest

--- tests/Unit/DomainRouterServiceTest.php
+++ tests/Unit/DomainRouterServiceTest.php
@@ -1,17 +1,23 @@
 <?php
 
-use App\Http\Services\DomainRouter;
 use App\Enums\Environment;
+use App\Http\Services\DomainRouter;
 use Illuminate\Http\Request;
 
-it('generates routes for production environment', function () {
-    $request = mock(Request::class);
-    $request->shouldReceive('host')->andReturn('example.ir');
-    $request->shouldReceive('schemeAndHttpHost')->andReturn('https://example.ir');
-    $request->shouldReceive('httpHost')->andReturn('example.ir');
+beforeEach(function () {
+    $this->request = mock(Request::class);
 
-    $router = new DomainRouter();
-    $routes = $router->generateRoutes($request);
+    // define system under test
+    $this->sut = new DomainRouter;
+});
+
+it('generates routes for production environment', function () {
+
+    $this->request->shouldReceive('host')->andReturn('example.ir');
+    $this->request->shouldReceive('schemeAndHttpHost')->andReturn('https://example.ir');
+    $this->request->shouldReceive('httpHost')->andReturn('example.ir');
+
+    $routes = $this->sut->generateRoutes($this->request);
 
     expect($routes)->toBe([
         'api' => 'https://example.ir/api',
@@ -22,13 +28,12 @@ it('generates routes for production environment', function () {
 });
 
 it('generates routes for staging environment', function () {
-    $request = mock(Request::class);
-    $request->shouldReceive('host')->andReturn('api.dev.example.com');
-    $request->shouldReceive('schemeAndHttpHost')->andReturn('https://api.dev.example.com');
-    $request->shouldReceive('httpHost')->andReturn('api.dev.example.com');
 
-    $router = new DomainRouter();
-    $routes = $router->generateRoutes($request);
+    $this->request->shouldReceive('host')->andReturn('api.dev.example.com');
+    $this->request->shouldReceive('schemeAndHttpHost')->andReturn('https://api.dev.example.com');
+    $this->request->shouldReceive('httpHost')->andReturn('api.dev.example.com');
+
+    $routes = $this->sut->generateRoutes($this->request);
 
     expect($routes)->toBe([
         'api' => 'https://api.dev.example.com/api',
@@ -39,13 +44,12 @@ it('generates routes for staging environment', function () {
 });
 
 it('generates routes for local environment', function () {
-    $request = mock(Request::class);
-    $request->shouldReceive('host')->andReturn('localhost');
-    $request->shouldReceive('schemeAndHttpHost')->andReturn('http://localhost:9000');
-    $request->shouldReceive('httpHost')->andReturn('localhost');
 
-    $router = new DomainRouter();
-    $routes = $router->generateRoutes($request);
+    $this->request->shouldReceive('host')->andReturn('localhost');
+    $this->request->shouldReceive('schemeAndHttpHost')->andReturn('http://localhost:9000');
+    $this->request->shouldReceive('httpHost')->andReturn('localhost');
+
+    $routes = $this->sut->generateRoutes($this->request);
 
     expect($routes)->toBe([
         'api' => 'http://localhost:9000/api',
