41d2cd8819f2eaead13d3f2911a1f31e66be371f
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-19 01:00:30 +0330

    :bug: comment unused reset password front url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,9 +29,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+        /*
+        ResetPassword::createUrlUsing(static function (User $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+        */
 
         Model::automaticallyEagerLoadRelationships();
 
