30f58f2ad6f34d75564964d2eca34f13c6109e90
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-19 23:03:58 +0330

    :recycle: add test for image store

--- tests/EndToEnd/Api/ImageTest.php
+++ tests/EndToEnd/Api/ImageTest.php
@@ -1,3 +1,31 @@
 <?php
 
-it('', function () {});
+use Illuminate\Http\UploadedFile;
+use App\Enums\ImageUploadMethod;
+
+it('can upload an image', function () {
+    $file = UploadedFile::fake()->image('test.jpg', 100, 100);
+
+    $payload = [
+        'image' => $file,
+        'directory' => '/uploads',
+        'upload_method' => ImageUploadMethod::METHOD_FIT_AND_SAVE->value,
+        'width' => 100,
+        'height' => 100,
+    ];
+
+    $response = $this->postJson(route('images.store'), $payload);
+    $response->assertOk();
+    expect($response->json())->not->toBeEmpty();
+});
+
+it('fails to upload without image', function () {
+    $payload = [
+        'directory' => 'test-images',
+        'upload_method' => ImageUploadMethod::METHOD_FIT_AND_SAVE->value,
+        'width' => 100,
+        'height' => 100,
+    ];
+    $response = $this->postJson(route('images.store'), $payload);
+    $response->assertUnprocessable();
+});
