23f2d5a535aba338ca2eaa66ad7cc1ec068f815d
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-25 11:31:27 +0330

    :recycle: refactor HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,40 +22,35 @@ class HomeController extends Controller
     {
         mongo_info('view', ['ip' => request()->ip(), 'url' => request()->url()], true);
 
-        // test jobs, ...
         try {
+            // sample jobs, ...
             PackageSent::dispatch('processed', 'prosper');
             PackageSent::dispatch('delivered', 'olamide');
 
-            //            $this->sendEmail();
-            $this->sendSms();
+            // sample send sms
+            $data = new \Amiriun\SMS\DataContracts\SendSMSDTO;
+            $data->setSenderNumber('300024444'); // also this can be set as default in config/sms.php
+            $data->setMessage('Hello, this is a test');
+            $data->setTo('09123000000');
+            app(SMSService::class)->send($data);
+
+            // sample send email
+            Mail::to('test@example.com')
+                ->when(false)
+                ->send(new UserLandMail(
+                    subject: 'welcome',
+                    from: getenv('MAIL_FROM_ADDRESS'),
+                    details: [
+                        'subject' => 'test',
+                        'body' => 'test',
+                    ],
+                    files: [],
+                ));
+
         } catch (\Exception $e) {
             \Log::error($e->getMessage());
         }
 
         return view('welcome');
-    }
-
-    private function sendEmail(): void
-    {
-        Mail::to('test@example.com')->send(new UserLandMail(
-            subject: 'welcome',
-            from: getenv('MAIL_FROM_ADDRESS'),
-            details: [
-                'subject' => 'test',
-                'body' => 'test',
-            ],
-            files: [],
-        ));
-    }
-
-    private function sendSms(): SentSMSOutputDTO
-    {
-        $data = new \Amiriun\SMS\DataContracts\SendSMSDTO;
-        $data->setSenderNumber('300024444'); // also this can be set as default in config/sms.php
-        $data->setMessage('Hello, this is a test');
-        $data->setTo('09123000000');
-
-        return app(SMSService::class)->send($data);
     }
 }
