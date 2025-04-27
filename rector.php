a9832249960ebfe9a947132a3b81ae1612df230a
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-04-28 00:00:35 +0330

    :art: modify rector paths

--- rector.php
+++ rector.php
@@ -8,12 +8,8 @@ use RectorLaravel\Set\LaravelSetList;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
-        __DIR__.'/config',
-        __DIR__.'/lang',
+        __DIR__.'/database',
         __DIR__.'/modules',
-        __DIR__.'/public',
-        __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
     ])
