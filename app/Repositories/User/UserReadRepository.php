d9a1be1565d9977f3e285045199986f467e5b28b
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-01 00:31:03 +0330

    :art: use BaseRepository and refactor UserReadRepository

--- app/Repositories/User/UserReadRepository.php
+++ app/Repositories/User/UserReadRepository.php
@@ -2,15 +2,30 @@
 
 namespace App\Repositories\User;
 
+use App\Abstracts\BaseRepository;
 use App\Models\Advertise\Advertisement;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 
-class UserReadRepository
+class UserReadRepository extends BaseRepository
 {
+    public function findById(int $id): ?User
+    {
+        return $this->baseQuery()
+            ->where('id', $id)
+            ->first();
+    }
+
+    public function getUserByMobile(string $mobile): ?User
+    {
+        return $this->freshQuery()->getQuery()->where('mobile', $mobile)->first();
+    }
+
     public function getUsersWithLatestAdvertisementPostedDate(): Collection
     {
-        return User::query()
+        return $this->freshQuery()->getQuery()
+            ->select('*') // only get required fields
             ->addSelect([
                 'latest_advertisement_posted_at' => Advertisement::query()
                     ->select('created_at')
@@ -20,5 +35,10 @@ class UserReadRepository
             ])
             ->take(1000)
             ->get();
+    }
+
+    protected function baseQuery(): Builder
+    {
+        return User::query();
     }
 }
