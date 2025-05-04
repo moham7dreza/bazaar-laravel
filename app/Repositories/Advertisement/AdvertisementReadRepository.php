4be69869574d15e0c446b57d0db5fbe33781b67a
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-04 23:29:59 +0330

    :art: add method for AdvertisementReadRepository to get ads from users to show users with avatars

--- app/Repositories/Advertisement/AdvertisementReadRepository.php
+++ app/Repositories/Advertisement/AdvertisementReadRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Advertisement;
 
 use App\Abstracts\BaseRepository;
 use App\Data\DTOs\PaginatedListViewDTO;
+use App\Enums\UserId;
 use App\Models\Advertise\Advertisement;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -14,8 +15,32 @@ class AdvertisementReadRepository extends BaseRepository
         $items = $this->freshQuery()->getQuery()
             ->with('user:created_at')
             ->whereHas('user', fn (Builder $query) => $query->createdAfter($date))
+            ->active()
+            ->published()
             ->take($limit)
             ->latest()
+            ->paginate($perPage);
+
+        return new PaginatedListViewDTO($items);
+    }
+
+    public function getTopAdsOfDistinctUsersHaveAvatar(int $limit, int $perPage = 20): PaginatedListViewDTO
+    {
+        $items = $this->freshQuery()->getQuery()
+            ->select('user_id')
+            ->distinct('user_id')
+            ->whereRelation('user', function (Builder $query) {
+                $query
+                    ->whereNotIn('id', [
+                        UserId::Admin->value,
+                    ])
+                    ->whereNotNull('avatar_url');
+            })
+            ->with('user:id,avatar_url')
+            ->inRandomOrder()
+            ->active()
+            ->published()
+            ->take($limit)
             ->paginate($perPage);
 
         return new PaginatedListViewDTO($items);
