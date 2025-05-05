193d033b417edeae70270c4b6d80d26c74192d59
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-05 23:29:26 +0330

    :art: add OnlyAllowValidHostsMiddleware

---
+++ app/Http/Middleware/OnlyAllowValidHostsMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Responses\ApiJsonResponse;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class OnlyAllowValidHostsMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $trustedHosts = [
+            'http://bazaar.local',
+            'http://localhost:3000',
+        ];
+
+        if (! in_array($request->getHost(), $trustedHosts, true)) {
+
+            return ApiJsonResponse::error(Response::HTTP_NOT_FOUND, __('response.general.not-found'));
+        }
+
+        return $next($request);
+    }
+}
