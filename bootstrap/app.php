bf0d45923a609779471683f41783ebd14085ad49
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-08 11:04:59 +0330

    :art: comment RequireJsonMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -36,7 +36,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \App\Http\Middleware\RequireJsonMiddleware::class,
+            //            \App\Http\Middleware\RequireJsonMiddleware::class,
             \App\Http\Middleware\ApiRequestLoggerMiddleware::class,
             //            \App\Http\Middleware\OnlyAllowValidHostsMiddleware::class,
         ]);
