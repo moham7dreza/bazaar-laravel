77967f9b054fb1df1e6850755a2a8378d70f5970
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-08 22:24:38 +0330

    :art: define RegisterUserRequest and modify controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules;
 use Illuminate\Validation\ValidationException;
 
 class RegisteredUserController extends Controller
@@ -19,23 +18,14 @@ class RegisteredUserController extends Controller
      *
      * @throws ValidationException
      */
-    public function __invoke(Request $request): Response
+    public function __invoke(RegisterUserRequest $request): Response
     {
-
-        $request->validate([
-            'name'     => ['required', 'string', 'max:255'],
-            'email'    => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique('users', 'email')->withoutTrashed()],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'mobile'   => ['required', 'string', 'max:15', Rule::unique('users', 'mobile')->withoutTrashed()],
-            'city_id'  => ['nullable', Rule::exists('cities', 'id')->whereNull('deleted_at')],
-        ]);
-
         $user = User::create([
-            'name'     => $request->name,
-            'email'    => $request->email,
-            'password' => Hash::make($request->password),
-            'mobile'   => $request->mobile,
-            'city_id'  => $request->city_id,
+            'name'     => $request->str('name'),
+            'email'    => $request->str('email'),
+            'password' => Hash::make($request->str('password')),
+            'mobile'   => $request->str('mobile'),
+            'city_id'  => $request->get('city_id'),
         ]);
 
         event(new Registered($user));
