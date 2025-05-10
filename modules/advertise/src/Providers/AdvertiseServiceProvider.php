d09bfb3261f5ca4c88ede2357f7a488b5b97eb6f
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-10 23:04:37 +0330

    :sparkles: add new advertise module

---
+++ modules/advertise/src/Providers/AdvertiseServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Advertise\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+final class AdvertiseServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
---
+++ modules/advertise/tests/Feature/Providers/AdvertiseServiceProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Advertise\Tests;
+
+use Tests\TestCase;
+
+final class AdvertiseServiceProviderTest extends TestCase
+{
+    // TODO
+}
