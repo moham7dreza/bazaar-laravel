a90e376aedb250ae7d4db430127d68e82f369dbb
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-11 23:47:59 +0330

    :art: register ad policy in provider

--- modules/advertise/src/Providers/AdvertiseServiceProvider.php
+++ modules/advertise/src/Providers/AdvertiseServiceProvider.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Modules\Advertise\Providers;
 
 use Database\Seeders\DatabaseSeeder;
+use Gate;
 use Illuminate\Support\ServiceProvider;
 use Modules\Advertise\Commands\AdvertisementLadderCommand;
 use Modules\Advertise\Database\Seeders\AdvertiseSeeder;
+use Modules\Advertise\Models\Advertisement;
+use Modules\Advertise\Policies\AdvertisementPolicy;
 
 final class AdvertiseServiceProvider extends ServiceProvider
 {
@@ -24,10 +27,16 @@ final class AdvertiseServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->configurePolicies();
     }
 
     private function setupSeeders(): void
     {
         DatabaseSeeder::$seeders[] = AdvertiseSeeder::class;
+    }
+
+    private function configurePolicies(): void
+    {
+        Gate::policy(Advertisement::class, AdvertisementPolicy::class);
     }
 }
