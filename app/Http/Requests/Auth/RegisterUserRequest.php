500fc21ceb36b37a919f59ac4fef59be1c931e1d
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-11 22:49:40 +0330

    :art: move RegisterUserRequest to Auth dir

--- app/Http/Requests/Auth/RegisterUserRequest.php
+++ app/Http/Requests/Auth/RegisterUserRequest.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App\Http\Requests;
+declare(strict_types=1);
+
+namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
 
-class RegisterUserRequest extends FormRequest
+final class RegisterUserRequest extends FormRequest
 {
     public function authorize(): bool
     {
