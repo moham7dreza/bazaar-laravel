5e27e93f80a0573852b67cf4fa1d1d5d27a354f3
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-13 23:44:06 +0330

    :art: improve app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Responses\ApiJsonResponse;
 use BezhanSalleh\FilamentExceptions\FilamentExceptions;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
@@ -14,10 +17,10 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     //        then: function () {
     //            Route::middleware('api')
@@ -29,31 +32,30 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
 
         $middleware->append([
-            \App\Http\Middleware\UserCheckSuspendedMiddleware::class,
-            \App\Http\Middleware\EnableDebugForDeveloper::class,
-            \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+            App\Http\Middleware\UserCheckSuspendedMiddleware::class,
+            App\Http\Middleware\EnableDebugForDeveloper::class,
+            Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         ]);
 
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             //            \App\Http\Middleware\RequireJsonMiddleware::class,
-            \App\Http\Middleware\ApiRequestLoggerMiddleware::class,
+            App\Http\Middleware\ApiRequestLoggerMiddleware::class,
             //            \App\Http\Middleware\OnlyAllowValidHostsMiddleware::class,
         ]);
 
         $middleware->alias([
-            'verified'           => \App\Http\Middleware\EnsureEmailIsVerified::class,
-            'mobileVerified'     => \App\Http\Middleware\EnsureMobileIsVerified::class,
-            'admin'              => \App\Http\Middleware\CheckAdminMiddleware::class,
-            'dev'                => \App\Http\Middleware\OnlyAllowDevelopersMiddleware::class,
-            'abilities'          => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
-            'ability'            => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
-            'role'               => \Spatie\Permission\Middleware\RoleMiddleware::class,
-            'permission'         => \Spatie\Permission\Middleware\PermissionMiddleware::class,
-            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'verified'           => App\Http\Middleware\EnsureEmailIsVerified::class,
+            'mobileVerified'     => App\Http\Middleware\EnsureMobileIsVerified::class,
+            'admin'              => App\Http\Middleware\CheckAdminMiddleware::class,
+            'dev'                => App\Http\Middleware\OnlyAllowDevelopersMiddleware::class,
+            'abilities'          => Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
+            'ability'            => Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
+            'role'               => Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission'         => Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
 
-        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
@@ -72,37 +74,45 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->renderable(function (Throwable $e) {
 
             // Authorization
-            if ($e instanceof AuthorizationException) {
+            if ($e instanceof AuthorizationException)
+            {
                 return ApiJsonResponse::error(Response::HTTP_FORBIDDEN, 'AuthorizationException');
             }
 
             // Access Denied
-            if ($e instanceof AccessDeniedHttpException) {
+            if ($e instanceof AccessDeniedHttpException)
+            {
                 return ApiJsonResponse::error(Response::HTTP_FORBIDDEN, 'AccessDeniedHttpException');
+            }
+
+            // Auth
+            if ($e instanceof AuthenticationException)
+            {
+//                return ApiJsonResponse::error(Response::HTTP_UNAUTHORIZED, 'Unauthenticated');
             }
 
             // Model Not Found
             $previous = $e->getPrevious();
-            if ($previous instanceof ModelNotFoundException) {
+            if ($previous instanceof ModelNotFoundException)
+            {
                 $model = str($previous->getModel())->afterLast('\\');
 
-                $message = isEnvProduction() ? 'Record Not Found.' : "$model Not Found.";
+                $message = isEnvProduction() ? 'Record Not Found.' : "{$model} Not Found.";
 
                 return ApiJsonResponse::error(Response::HTTP_NOT_FOUND, $message);
             }
 
             // Database
-            if ($e instanceof QueryException) {
-
+            if ($e instanceof QueryException)
+            {
                 return ApiJsonResponse::error(Response::HTTP_INTERNAL_SERVER_ERROR, 'QueryException');
             }
 
             // for other exceptions
-            /*
-            if (! $e instanceof ValidationException) {
-                return ApiJsonResponse::error(Response::HTTP_INTERNAL_SERVER_ERROR, 'Server Error');
+            if ( ! $e instanceof ValidationException)
+            {
+//                return ApiJsonResponse::error(Response::HTTP_INTERNAL_SERVER_ERROR, 'Server Error');
             }
-            */
         });
 
     })
