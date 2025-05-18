76c86e04ccb82ff9a4b8af1f7f781cb903feb54b
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-18 21:38:03 +0330

    :art: improve EnableDebugForDeveloper middleware

--- app/Http/Middleware/EnableDebugForDeveloper.php
+++ app/Http/Middleware/EnableDebugForDeveloper.php
@@ -1,18 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class EnableDebugForDeveloper
+final class EnableDebugForDeveloper
 {
     public function handle(Request $request, Closure $next): Response
     {
         $user = getUser();
-        if ($user && in_array($user->mobile, config('developer.backends'), true)) {
-            config(['app.debug' => true]);
+        if ($user && in_array($user->mobile, config('developer.backends'), true))
+        {
+            config()?->set('app.debug', true);
         }
 
         return $next($request);
