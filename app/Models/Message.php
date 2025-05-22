0fb77c1ed06eba2e7af1cb9bf28d6ef8b7f1a326
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-22 13:31:09 +0330

    :art: add Message model

---
+++ app/Models/Message.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use App\Models\Scopes\LatestScope;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Attributes\ScopedBy;
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#[ScopedBy([LatestScope::class])]
+#[UseFactory(MessageFactory::class)]
+class Message extends Model
+{
+    use Prunable;
+    // _____________________________________________ use section ________________________________________________
+    //
+    use SoftDeletes;
+
+    // _____________________________________________ props section ______________________________________________
+
+    protected $guarded = ['id'];
+
+    public function prunable(): Builder
+    {
+        return static::query()->where('created_at', '<=', now()->subMonths(6));
+    }
+
+    #[Scope]
+    public function active(): Builder
+    {
+        return $this->where('status', true);
+    }
+
+    // _____________________________________________ model related methods section ______________________________
+
+    protected function casts(): array
+    {
+        return [
+
+        ];
+    }
+
+    // _____________________________________________ relations section __________________________________________
+
+    // _____________________________________________ method section __________________________________________
+
+}
