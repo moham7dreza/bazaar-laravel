45d5ecfb5451b839cb44a5b41e03d1da6c1baa0a
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-05-29 22:56:32 +0330

    :art: add default item to request enums

--- modules/advertise/src/Http/Requests/App/AdvertisementGridViewRequest.php
+++ modules/advertise/src/Http/Requests/App/AdvertisementGridViewRequest.php
@@ -30,7 +30,7 @@ final class AdvertisementGridViewRequest extends FormRequest
     {
         return new AdvertisementSearchDTO(
             phrase: $this->str('phrase')->value(),
-            sort: $this->enum('sort', Sort::class),
+            sort: $this->enum('sort', Sort::class, Sort::NEWEST),
             perPage: $this->integer('per_page', 24),
             ids: $this->array('ids'),
         );
