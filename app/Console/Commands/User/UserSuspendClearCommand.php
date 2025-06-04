0fd3684c49a20f7d514bd754cf24afa4143a3914
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-06-04 17:45:57 +0330

    :art: improve UserSuspendClearCommand

--- app/Console/Commands/User/UserSuspendClearCommand.php
+++ app/Console/Commands/User/UserSuspendClearCommand.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands\User;
 
 use App\Jobs\UserSuspendClearJob;
@@ -9,26 +11,12 @@ use Illuminate\Support\Collection;
 
 class UserSuspendClearCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'user:suspend-clear';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Automatically lift expired suspensions';
 
-    /**
-     * Execute the console command.
-     */
     public function handle(): int
     {
-        /*
         // lazy collection allow you to process large datasets with minimal memory usage
         // as they load data only when needed
         LazyCollection::make(static function () {
@@ -37,18 +25,17 @@ class UserSuspendClearCommand extends Command
                 ->whereNotNull('suspended_until')
                 ->where('suspended_until', '<=', now());
 
-        })->each(function (User $user) {
+        })->each(function (User $user): void {
 
             UserSuspendClearJob::dispatch($user->pluck('id'));
         });
-        */
 
-        User::query()
-            ->whereNotNull('suspended_until')
-            ->where('suspended_until', '<=', now())
-            ->chunkById(100, function (Collection $users): void {
-                UserSuspendClearJob::dispatch($users->pluck('id'));
-            });
+        //        User::query()
+        //            ->whereNotNull('suspended_until')
+        //            ->where('suspended_until', '<=', now())
+        //            ->chunkById(100, function (Collection $users): void {
+        //                UserSuspendClearJob::dispatch($users->pluck('id'));
+        //            });
 
         $this->info('Expired suspensions have been lifted.');
 
