049d8506089b662bc1069b6c6b6dbf35e91b1bf0
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-06-04 09:42:57 +0330

    :sparkles: add abstract Action class

---
+++ app/Actions/Abstracts/Action.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Abstracts;
+
+abstract class Action
+{
+    abstract public function handle(): mixed;
+
+    public static function dispatch(mixed ...$params): mixed
+    {
+        $action = static::make(...$params);
+
+        if ($action->authorize())
+        {
+            return $action->handle();
+        }
+
+        return null;
+    }
+
+    public static function make(mixed ...$params): mixed
+    {
+        return new static(...$params);
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
