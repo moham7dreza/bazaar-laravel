a45a8d5210627a172c740845968537c0d5477702
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-06-06 01:10:05 +0330

    :art: improve AdvertisementSearchService and do not use filters for now because sending dto to filters from pipeline make code complex

--- modules/advertise/src/Services/AdvertisementSearchService.php
+++ modules/advertise/src/Services/AdvertisementSearchService.php
@@ -4,27 +4,37 @@ declare(strict_types=1);
 
 namespace Modules\Advertise\Services;
 
+use App\Contracts\Filter;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Pipeline\Pipeline;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Pipeline;
 use Modules\Advertise\DataContracts\AdvertisementSearchDTO;
+use Modules\Advertise\Http\Filters\FilterAdvertisementsByPhrase;
 use Modules\Advertise\Models\Advertisement;
-use Modules\Advertise\Pipelines\FilterAdvertisementsByPhrase;
+use phpDocumentor\Reflection\Types\ClassString;
 
 final class AdvertisementSearchService
 {
-    public function getAdvertisements(Builder $builder, AdvertisementSearchDTO $searchDTO)
+    public function getAdvertisements(Builder $builder, AdvertisementSearchDTO $searchDTO): LengthAwarePaginator
     {
-        return app(Pipeline::class)
-            ->send($builder)
-            ->through([
-                FilterAdvertisementsByPhrase::class,
-            ])
+        /** @var array<ClassString<Filter>> $filters */
+        $filters = [
+            //            FilterAdvertisementsByPhrase::class,
+        ];
+
+        return Pipeline::send($builder)
+            ->through($filters)
             ->finally(function (): void {
                 info('search request log.', request()->all());
             })
             ->then(fn (Builder $query) => $query
                 ->active()
                 ->published()
+                ->when($searchDTO->phrase, fn (Builder|Advertisement $builder) => $builder->whereAny([
+                    'title',
+                    'description',
+                    'tags',
+                ], 'like', '%' . $searchDTO->phrase . '%'))
                 ->when($searchDTO->ids, fn (Builder|Advertisement $builder) => $builder->whereIn('id', $searchDTO->ids))
                 ->when($searchDTO->sort, fn (Builder|Advertisement $builder) => $builder->sortBy($searchDTO->sort))
                 ->paginate($searchDTO->perPage)
