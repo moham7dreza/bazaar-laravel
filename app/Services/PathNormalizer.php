c64a59657f967d142bc9348a66c4e8ca83834d84
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-06-06 16:17:31 +0330

    :art: add PathNormalizer.php

---
+++ app/Services/PathNormalizer.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+class PathNormalizer
+{
+    public function standardizeProtocol(string $url, string $protocol = 'https')
+    {
+        // Replace http:// with https://
+        $url = Str::replaceStart('http://', $protocol . '://', $url);
+
+        // Add protocol if missing
+        if ( ! str_starts_with($url, $protocol . '://'))
+        {
+            $url = $protocol . '://' . $url;
+        }
+
+        return $url;
+    }
+
+    public function unifySubdomain(string $url, string $targetDomain)
+    {
+        // Replace www with target domain
+        $url = Str::replaceStart('www.', $targetDomain . '.', $url);
+
+        // Replace staging with target domain
+        $url = Str::replaceStart('staging.', $targetDomain . '.', $url);
+
+        return $url;
+    }
+
+    public function normalizeApiPath(string $endpoint)
+    {
+        // Remove leading /api if present
+        return Str::replaceStart('/api', '', $endpoint);
+    }
+}
