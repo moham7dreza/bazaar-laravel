30f5c003a2c9730a02ae5fd734f606544b9a277a
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-06-14 11:53:08 +0330

    :art: add ConfigurationManager

---
+++ app/Helpers/ConfigurationManager.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+final readonly class ConfigurationManager
+{
+    public function processSettings($settings)
+    {
+        $array = Arr::from($settings);
+
+        return $this->validateSettings($array);
+    }
+
+    public function combineConfigurations(...$configs)
+    {
+        return collect($configs)
+            ->map(fn ($config) => Arr::from($config))
+            ->reduce(fn ($carry, $item) => array_merge($carry, $item), []);
+    }
+
+    public function sanitizeAndStore($configuration, array $schema)
+    {
+        $data = Arr::from($configuration);
+
+        $validator = Validator::make($data, $schema);
+
+        if ($validator->fails())
+        {
+            throw new ValidationException($validator);
+        }
+
+        return $data;
+    }
+
+    private function validateSettings(array $settings)
+    {
+        return true;
+    }
+}
