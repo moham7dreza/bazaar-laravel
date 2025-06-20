2375c150a17dd8bdb3a09472e2fb4490fa44f1c2
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-06-20 17:44:05 +0330

    :art: add missing user info in UserResource.php

--- app/Http/Resources/Admin/User/UserResource.php
+++ app/Http/Resources/Admin/User/UserResource.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Resources\Admin\User;
 
 use Illuminate\Http\Request;
@@ -7,20 +9,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'id'                 => $this->id,
+            'name'               => $this->name,
+            'email'              => $this->email,
+            'email_verified_at'  => $this->email_verified_at,
+            'mobile'             => $this->mobile,
+            'mobile_verified_at' => $this->mobile_verified_at,
+            'user_type'          => $this->user_type,
+            'created_at'         => $this->created_at,
+            'updated_at'         => $this->updated_at,
         ];
     }
 }
