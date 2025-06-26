4d0ae60b56f2345580923d45a215a2df5a7c0a2c
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2025-06-26 15:40:38 +0330

    :art: add missing sms.php lang files

--- modules/monitoring/src/Providers/CommandLoggingServiceProvider.php
+++ modules/monitoring/src/Providers/CommandLoggingServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Monitoring\Providers;
 
-use Carbon\Carbon;
 use Exception;
 use Illuminate\Console\Events;
 use Illuminate\Database\Events\QueryExecuted;
