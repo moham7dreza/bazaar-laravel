68eea98c98b01744367d418095ae96a148540fcf
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2025-07-05 14:30:20 +0330

    :art: http retry

--- tests/EndToEnd/Api/ApiTest.php
+++ tests/EndToEnd/Api/ApiTest.php
@@ -1,14 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Http;
 
-it('test next js api', function () {
+it('test next js api', function (): void {
 
     Http::dd()
+        ->retry(3, 100)
         ->withUrlParameters([
-            'base' => config()->string('app.frontend_url'),
-            'version' => 'v1',
-            'userId' => 123,
+            'base'     => config()->string('app.frontend_url'),
+            'version'  => 'v1',
+            'userId'   => 123,
             'endpoint' => 'profile',
         ])
         ->get('{+base}/{version}/users/{userId}/{endpoint}?include=settings');
