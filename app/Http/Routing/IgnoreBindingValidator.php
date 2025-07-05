b296652cfc9627e6a0973a76053e716a9621c665
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2025-07-05 13:16:45 +0330

    :art: use rescue instead of try catch

--- app/Http/Routing/IgnoreBindingValidator.php
+++ app/Http/Routing/IgnoreBindingValidator.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Routing;
 
 use Illuminate\Contracts\Routing\Registrar;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Matching\ValidatorInterface;
 use Illuminate\Routing\Route;
@@ -23,14 +22,10 @@ class IgnoreBindingValidator implements ValidatorInterface
 
         $route = (clone $route)->bind($request);
 
-        try
-        {
+        rescue(function () use ($router, $route): void {
             $router->substituteBindings($route);
             $router->substituteImplicitBindings($route);
-        } catch (ModelNotFoundException $e)
-        {
-            return false;
-        }
+        }, false);
 
         return true;
     }
