dc1d42cb7c5330e82ce8b101cf9d8765a7cb1e2d
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2025-07-05 13:00:31 +0330

    :art: add Sort.php for ordering

---
+++ app/Http/Orders/Sort.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Orders;
+
+use Illuminate\Database\Query\Builder;
+
+final class Sort
+{
+    // flights/123/reservations?sort=seatNumber,-createdAt
+    public static function apply(
+        ?string $sort,
+        Builder $query,
+        string $tableName,
+        array $supportedFields,
+    ): void {
+        if (blank($sort))
+        {
+            return;
+        }
+
+        $sortFields = explode(',', $sort);
+
+        foreach ($sortFields as $sortField)
+        {
+            $sortOrder = 'ASC';
+
+            if ('-' === $sortField[0])
+            {
+                $sortOrder = 'DESC';
+                $sortField = mb_substr($sortField, 1);
+            }
+
+            if (in_array($sortField, $supportedFields, true))
+            {
+                $query->orderBy("{$tableName}.{$sortField}", $sortOrder);
+            }
+        }
+    }
+}
