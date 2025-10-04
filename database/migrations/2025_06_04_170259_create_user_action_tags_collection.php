9895a8e361ba72dd7248f5642f3e5ec531991def
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-04 21:54:04 +0330

    :bug: add shouldRun condition to 2025_06_04_170259_create_user_action_tags_collection.php

--- database/migrations/2025_06_04_170259_create_user_action_tags_collection.php
+++ database/migrations/2025_06_04_170259_create_user_action_tags_collection.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 
 return new class() extends Migration {
+    public function shouldRun(): bool
+    {
+        return (bool) DB::connection('mongodb')->getPdo();
+    }
+
     public function up(): void
     {
         DB::connection('mongodb')->getCollection('user_action_tags')
