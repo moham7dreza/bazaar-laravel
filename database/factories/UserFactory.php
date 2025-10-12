150d6cbd5cb900db90b7d4684881bf4ae79fbf01
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-12 22:34:01 +0330

    :art: remove unnecessary password hash in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,16 +9,12 @@ use App\Enums\Theme;
 use App\Models\Geo\City;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Random\RandomException;
 
 class UserFactory extends Factory
 {
-    protected static ?string $password;
-
     /**
      * @throws RandomException
      */
@@ -28,7 +24,7 @@ class UserFactory extends Factory
             'name'               => fake()->name(),
             'email'              => fake()->unique()->safeEmail(),
             'email_verified_at'  => now(),
-            'password'           => static::$password ??= Hash::make('password'),
+            'password'           => 'password',
             'remember_token'     => Str::random(10),
             'theme'              => Theme::DRACULA->value,
             'suspended_at'       => null,
