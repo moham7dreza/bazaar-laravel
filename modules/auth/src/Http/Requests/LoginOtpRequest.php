8c1e7ef19ea465e9e945a421d140119b00e93987
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-15 23:55:11 +0330

    :art: add missing mobile exists validation to LoginOtpRequest.php

--- modules/auth/src/Http/Requests/LoginOtpRequest.php
+++ modules/auth/src/Http/Requests/LoginOtpRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Auth\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 final class LoginOtpRequest extends FormRequest
 {
@@ -16,7 +17,7 @@ final class LoginOtpRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'mobile' => ['required', 'string', 'max:15'],
+            'mobile' => ['required', 'string', 'max:15', Rule::exists('users', 'mobile')],
         ];
     }
 }
