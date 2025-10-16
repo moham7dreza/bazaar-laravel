0bdb853450abbb258742f54a3a990918f8ded760
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-17 02:52:22 +0330

    :art: fix RegistrationTest

--- modules/auth/tests/EndToEnd/Web/RegistrationTest.php
+++ modules/auth/tests/EndToEnd/Web/RegistrationTest.php
@@ -6,8 +6,8 @@ test('new users can register', function (): void {
     $response = \Pest\Laravel\post('/register', [
         'name'                  => 'Test User',
         'email'                 => 'test@example.com',
-        'password'              => 'password',
-        'password_confirmation' => 'password',
+        'password'              => $pass = 'asdasdsa@1231DQWD',
+        'password_confirmation' => $pass,
         'mobile'                => '09141234567',
     ]);
 
