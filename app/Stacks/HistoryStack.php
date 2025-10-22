51e51e88274c4ddc1e57387a6bd5321983eb719c
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-23 00:11:00 +0330

    :art: add HistoryStack to implement undo/redo functionality (user actions)using stack with redis

---
+++ app/Stacks/HistoryStack.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Stacks;
+
+use http\Exception\InvalidArgumentException;
+use JsonException;
+use Redis;
+
+class HistoryStack
+{
+    public function __construct(
+        private readonly Redis $redis,
+    ) {
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function push(array $event, int $userId): void
+    {
+        $this->redis->lPush(
+            "history:todos:{$event['data']['todo_id']}:{$userId}",
+            json_encode($event, JSON_THROW_ON_ERROR)
+        );
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function pop(int $todoId, int $userId): array
+    {
+        $eventJson = $this->redis->lpop("history:todos:{$todoId}:{$userId}");
+
+        if ( ! $eventJson)
+        {
+            throw new InvalidArgumentException('Event not found in redis');
+        }
+
+        return json_decode($eventJson, true, 512, JSON_THROW_ON_ERROR);
+    }
+}
