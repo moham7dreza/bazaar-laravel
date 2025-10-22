793cc3d65330a055c183d5088868a605e7f45066
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-22 21:31:00 +0330

    :art: put password column as invisible

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->invisible();
             $table->rememberToken();
             $table->timestamps();
         });
