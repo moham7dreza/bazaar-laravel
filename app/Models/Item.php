1346505c6eebd00d7617982fc86ab66fe15a3fd6
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-23 15:42:21 +0330

    :art: remove unused prunable from Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -9,21 +9,14 @@ use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 #[ScopedBy([LatestScope::class])]
 class Item extends Model
 {
-    use Prunable;
     use SoftDeletes;
 
     protected $guarded = ['id'];
-
-    public function prunable(): Builder
-    {
-        return static::query()->where('created_at', '<=', now()->subMonths(6));
-    }
 
     #[Scope]
     protected function active(): Builder
