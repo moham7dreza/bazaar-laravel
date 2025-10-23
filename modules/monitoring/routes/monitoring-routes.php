e9a07bf926f8e98fa1c5a29f79323abe73529339
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-23 15:20:53 +0330

    :art: temporary disable monitoring-routes middlewares

--- modules/monitoring/routes/monitoring-routes.php
+++ modules/monitoring/routes/monitoring-routes.php
@@ -6,15 +6,17 @@ use Modules\Monitoring\Http\Controllers\HealthCheckController;
 use Spatie\Health\Http\Controllers as HeathCheckControllers;
 
 Route::middleware([
+    /*
     App\Http\Middleware\OnlyAllowDevelopersMiddleware::class,
     App\Http\Middleware\CheckAdminMiddleware::class,
+    */
 ])
     ->group(function (): void {
 
         Route::can('viewHealth')->group(function (): void {
 
             Route::get('health', HeathCheckControllers\HealthCheckResultsController::class);
-            Route::get('health-json', HeathCheckControllers\HealthCheckJsonResultsController::class);
-            Route::get('health-custom', HealthCheckController::class)->name('web.health-custom');
+//            Route::get('health-json', HeathCheckControllers\HealthCheckJsonResultsController::class);
+//            Route::get('health-custom', HealthCheckController::class)->name('web.health-custom');
         });
     });
