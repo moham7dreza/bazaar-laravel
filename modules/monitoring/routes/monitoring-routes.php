a97d5799229404b1a42d27d9ce551b24fb0728fd
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-10-29 02:53:19 +0330

    :art: fix monitoring-routes.php

--- modules/monitoring/routes/monitoring-routes.php
+++ modules/monitoring/routes/monitoring-routes.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Route;
 use Modules\Monitoring\Http\Controllers\HealthCheckController;
 use Spatie\Health\Http\Controllers as HeathCheckControllers;
 
-Illuminate\Support\Facades\Route::middleware([
+Route::middleware([
     /*
     App\Http\Middleware\OnlyAllowDevelopersMiddleware::class,
     App\Http\Middleware\CheckAdminMiddleware::class,
@@ -13,10 +14,10 @@ Illuminate\Support\Facades\Route::middleware([
 ])
     ->group(function (): void {
 
-        Illuminate\Support\Facades\Route::can('viewHealth')->group(function (): void {
+        Route::can('viewHealth')->group(function (): void {
 
-            Illuminate\Support\Facades\Route::get('health', HeathCheckControllers\HealthCheckResultsController::class);
-//            Route::get('health-json', HeathCheckControllers\HealthCheckJsonResultsController::class);
-//            Route::get('health-custom', HealthCheckController::class)->name('web.health-custom');
+            Route::get('health', HeathCheckControllers\HealthCheckResultsController::class);
+            Route::get('health-json', HeathCheckControllers\HealthCheckJsonResultsController::class);
+            Route::get('health-custom', HealthCheckController::class)->name('web.health-custom');
         });
     });
