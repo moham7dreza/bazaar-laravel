628284e263bab287a10755f2ac2f297343205e66
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2025-11-19 23:45:48 +0330

    :art: improve AdvertisementDiscountService

--- modules/advertise/src/Services/AdvertisementDiscountService.php
+++ modules/advertise/src/Services/AdvertisementDiscountService.php
@@ -4,20 +4,36 @@ declare(strict_types=1);
 
 namespace Modules\Advertise\Services;
 
+use Cknow\Money\Money;
 use Modules\Advertise\Models\Advertisement;
 
 class AdvertisementDiscountService
 {
-    public function getFormattedDiscount(
+    public function getDiscount(
         int $advertisementId,
         int $couponPercentage,
-    ): string {
-        $advertisement = Advertisement::query()
-            ->find($advertisementId, ['price']);
-
-        return money($advertisement->price)
+    ): Money {
+        return money($this->getAdvertisementPrice($advertisementId))
             ->multiply($couponPercentage)
-            ->divide(100)
-            ->format();
+            ->divide(100);
+    }
+
+    public function getFinalPrice(
+        int $advertisementId,
+        int $couponPercentage,
+    ): Money {
+        $advertisementPrice = $this->getAdvertisementPrice($advertisementId);
+
+        $discount = $this->getDiscount($advertisementId, $couponPercentage);
+
+        return money($advertisementPrice)
+            ->subtract($discount);
+    }
+
+    private function getAdvertisementPrice(int $advertisementId): int
+    {
+        return once(fn () => Advertisement::query()
+            ->whereKey($advertisementId)
+            ->value('price'));
     }
 }
