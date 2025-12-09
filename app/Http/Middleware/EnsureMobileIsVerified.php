e9aac26affd9d9972f307d4be547f7fbeff05d82
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-12-09 23:20:45 +0330

    :bug: fix return type

--- app/Http/Middleware/EnsureMobileIsVerified.php
+++ app/Http/Middleware/EnsureMobileIsVerified.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use App\Contracts\MustVerifyMobile;
+use App\Http\Responses\ApiJsonResponse;
 use Closure;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
@@ -20,14 +21,14 @@ final class EnsureMobileIsVerified
         return self::class . ':' . $route;
     }
 
-    public function handle(Request $request, Closure $next, ?string $redirectToRoute = null): JsonResponse|RedirectResponse
+    public function handle(Request $request, Closure $next, ?string $redirectToRoute = null): JsonResponse|RedirectResponse|Response
     {
         if ( ! $request->user() ||
             ($request->user() instanceof MustVerifyMobile &&
                 ! $request->user()->hasVerifiedMobile()))
         {
             return $request->expectsJson()
-                ? abort(Response::HTTP_FORBIDDEN, 'Your mobile number is not verified.')
+                ? ApiJsonResponse::error(Response::HTTP_FORBIDDEN, 'Your mobile number is not verified.')
                 : Redirect::guest(URL::route($redirectToRoute ?: 'verification.notice'));
         }
 
